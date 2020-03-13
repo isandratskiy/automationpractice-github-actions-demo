2ff01bd55dc09be4d6c33f64b8908bb0bc4cde36
isandratskiy <46830470+isandratskiy@users.noreply.github.com>
2020-03-13 02:14:07 +0200

    added -d for compose

--- selenoid_start.sh
+++ selenoid_start.sh
@@ -4,4 +4,4 @@ docker pull selenoid/vnc_chrome:80.0
 echo "==============================> Pulling Firefox"
 docker pull selenoid/vnc_firefox:72.0
 echo "Starting Selenoid Service ... "
-docker-compose up --force-recreate
+docker-compose up --force-recreate -d
