1e64fb6f40782ed22598a7ea039fef0faad08ed1
isandratskiy <i.sandrackii@gmail.com>
2020-04-05 01:21:22 +0300

    bump firefox ver

--- selenoid_start.sh
+++ selenoid_start.sh
@@ -2,6 +2,6 @@
 echo "==============================> Pulling Chrome" 
 docker pull selenoid/vnc_chrome:80.0
 echo "==============================> Pulling Firefox"
-docker pull selenoid/vnc_firefox:72.0
+docker pull selenoid/vnc_firefox:74.0
 echo "Starting Selenoid Service ... "
 docker-compose up --force-recreate -d
